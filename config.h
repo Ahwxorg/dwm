1c1ba0c28ea46dd7ed62b2000442fe8c89dd6e36
Ahwx <ahwx@ahwx.org>
2022-10-31 11:07:56 +0100

    Add keybinds and more stuff. Use with pywal since the default colors are awfull

--- config.h
+++ config.h
@@ -100,7 +100,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Right,  tagmon,         {.i = +1 } },
 	{ MODKEY,	                   		XK_minus,  setgaps,	       {.i = -1 } },
 	{ MODKEY,			                  XK_equal,  setgaps,   	   {.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_equal,  setgaps,	   {.i =  0 } },
+	{ MODKEY|ShiftMask,		          XK_equal,  setgaps,   	   {.i =  0 } },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
