49d7de8dfe377b9c830909aa74ee7e2d66501b2c
Ahwx <ahwx@ahwx.org>
2022-11-04 00:29:56 +0530

    fix laptop specific things

--- config.h
+++ config.h
@@ -46,9 +46,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[Tile]",      tile },    /* first entry is default */
-	{ "[Float]",      NULL },    /* no layout function means floating behavior */
-	{ "[Monocle]",      monocle },
+	{ " [Tile]",      tile },    /* first entry is default */
+	{ " [Float]",      NULL },    /* no layout function means floating behavior */
+	{ " [Monocle]",      monocle },
 };
 
 /* key definitions */
